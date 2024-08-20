90ac3b46b4a681a9610c416f32ed9fc2275d524c
Havyner Caetano <hrcgeo@gmail.com>
2024-08-20 09:26:26 -0300

    feat: Render all alarm options in the modal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,6 +14,43 @@ const DEFAULT_LONG_BREAK_SECOND = 10;
 
 Modal.setAppElement("#root");
 
+const alarmsMap = [
+  {
+    id: "e3b0c442-98fc-462d-a1e8-3bf0bb407ad5",
+    name: "Birds chirping",
+    src: "/alarms/birds-chirping.mp3"
+  },
+  {
+    id: "d0f5c79b-7bfb-4d1c-9837-4a7ffab39175",
+    name: "Game show winner",
+    src: "/alarms/game-show-winner-bell.mp3"
+  },
+  {
+    id: "4d92517f-e30b-4f19-9e84-0047d9c94d7e",
+    name: "Hotel desk bell",
+    src: "/alarms/hotel-desk-bell.mp3"
+  },
+  {
+    id: "0e7cfb6a-87a5-4374-a3cc-2a1ecbb2ef73",
+    name: "Large gong",
+    src: "/alarms/large-gong.mp3"
+  },
+  {
+    id: "c84f98d7-93f0-49d2-8c58-752a767d2402",
+    name: "Small gong",
+    src: "/alarms/small-gong.mp3"
+  },
+  {
+    id: "617bdd67-589f-4e27-94ad-d8d3994c1c29",
+    name: "Police siren",
+    src: "/alarms/police-siren.mp3"
+  },
+  {
+    id: "89e2a07f-fc7d-4c56-bc5a-024d21bbfc57",
+    name: "Train steam whistle",
+    src: "/alarms/train-steam-whistle.mp3"
+  }
+];
 const customStyles = {
   content: {
     top: "50%",
@@ -101,8 +138,11 @@ function Home() {
     setSecond(pomodoroSecond);
   };
 
-  const playSound = () => {
-    audioRef.current.play();
+  const playSound = (soundUrl = "/alarms/small-gong.mp3") => {
+    if (audioRef.current) {
+      audioRef.current.src = soundUrl;
+      audioRef.current.play();
+    }
   };
 
   const handleEndOfCycle = () => {
@@ -183,7 +223,21 @@ function Home() {
               </button>
             </section>
 
-            <div>I am a modal</div>
+            <p>Choose an alarm sound</p>
+            <div className="flex w-full flex-col">
+              {alarmsMap.map((alarm) => (
+                <div className="flex" key={alarm.id}>
+                  <p>{alarm.name}</p>
+                  <button onClick={() => playSound(alarm.src)}>
+                    <img
+                      src="play-button.png"
+                      alt={`Play ${alarm.name} sound`}
+                      width={24}
+                    />
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
         </Modal>
       </section>
@@ -211,7 +265,7 @@ function Home() {
           >
             Stop
           </button>
-          <audio ref={audioRef} src="/zen-gong.mp3" />
+          <audio ref={audioRef} src="/alarms/small-gong.mp3" />
         </section>
         <p>Pomodoro cycles: {pomodoroCycles}</p>
       </section>
