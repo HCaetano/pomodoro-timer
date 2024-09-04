be0809faf197bba624bc7150aedf83c25b0102cb
Havyner Caetano <hrcgeo@gmail.com>
2024-09-04 09:48:36 -0300

    feat: Extract constants from Home component to a separate file

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,54 +3,19 @@ import classNames from "classnames";
 import React, { useRef, useState } from "react";
 import Modal from "react-modal";
 import { Timer } from "../../components";
+import {
+  alarmsMap,
+  DEFAULT_LONG_BREAK_MINUTE,
+  DEFAULT_LONG_BREAK_SECOND,
+  DEFAULT_POMODORO_MINUTE,
+  DEFAULT_POMODORO_SECOND,
+  DEFAULT_SHORT_BREAK_MINUTE,
+  DEFAULT_SHORT_BREAK_SECOND
+} from "../../utils/constants";
 import styles from "./home.module.css";
-
-const DEFAULT_POMODORO_MINUTE = 1;
-const DEFAULT_POMODORO_SECOND = 6;
-const DEFAULT_SHORT_BREAK_MINUTE = 0;
-const DEFAULT_SHORT_BREAK_SECOND = 5;
-const DEFAULT_LONG_BREAK_MINUTE = 0;
-const DEFAULT_LONG_BREAK_SECOND = 10;
 
 Modal.setAppElement("#root");
 
-const alarmsMap = [
-  {
-    id: "e3b0c442-98fc-462d-a1e8-3bf0bb407ad5",
-    name: "Birds chirping",
-    src: "/alarms/birds-chirping.mp3"
-  },
-  {
-    id: "d0f5c79b-7bfb-4d1c-9837-4a7ffab39175",
-    name: "Game show winner",
-    src: "/alarms/game-show-winner-bell.mp3"
-  },
-  {
-    id: "4d92517f-e30b-4f19-9e84-0047d9c94d7e",
-    name: "Hotel desk bell",
-    src: "/alarms/hotel-desk-bell.mp3"
-  },
-  {
-    id: "0e7cfb6a-87a5-4374-a3cc-2a1ecbb2ef73",
-    name: "Large gong",
-    src: "/alarms/large-gong.mp3"
-  },
-  {
-    id: "c84f98d7-93f0-49d2-8c58-752a767d2402",
-    name: "Small gong",
-    src: "/alarms/small-gong.mp3"
-  },
-  {
-    id: "617bdd67-589f-4e27-94ad-d8d3994c1c29",
-    name: "Police siren",
-    src: "/alarms/police-siren.mp3"
-  },
-  {
-    id: "89e2a07f-fc7d-4c56-bc5a-024d21bbfc57",
-    name: "Train steam whistle",
-    src: "/alarms/train-steam-whistle.mp3"
-  }
-];
 const customStyles = {
   content: {
     top: "50%",
@@ -65,22 +30,32 @@ const customStyles = {
 };
 
 function Home() {
-  const [minute, setMinute] = useState(DEFAULT_POMODORO_MINUTE);
-  const [second, setSecond] = useState(DEFAULT_POMODORO_SECOND);
-  const [pomodoroMinute, setPomodoroMinute] = useState(DEFAULT_POMODORO_MINUTE);
-  const [pomodoroSecond, setPomodoroSecond] = useState(DEFAULT_POMODORO_SECOND);
-  const [breakMinute, setBreakMinute] = useState(DEFAULT_SHORT_BREAK_MINUTE);
-  const [breakSecond, setBreakSecond] = useState(DEFAULT_SHORT_BREAK_SECOND);
-  const [isRunning, setIsRunning] = useState(false);
-  const [isPomodoroActive, setIsPomodoroActive] = useState(true);
-  const [isBreakActive, setIsBreakActive] = useState(false);
-  const [pomodoroCycles, setPomodoroCycles] = useState(0);
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [alarmSource, setAlarmSource] = useState("/alarms/small-gong.mp3");
+  const [minute, setMinute] = useState<number>(DEFAULT_POMODORO_MINUTE);
+  const [second, setSecond] = useState<number>(DEFAULT_POMODORO_SECOND);
+  const [pomodoroMinute, setPomodoroMinute] = useState<number>(
+    DEFAULT_POMODORO_MINUTE
+  );
+  const [pomodoroSecond, setPomodoroSecond] = useState<number>(
+    DEFAULT_POMODORO_SECOND
+  );
+  const [breakMinute, setBreakMinute] = useState<number>(
+    DEFAULT_SHORT_BREAK_MINUTE
+  );
+  const [breakSecond, setBreakSecond] = useState<number>(
+    DEFAULT_SHORT_BREAK_SECOND
+  );
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [isPomodoroActive, setIsPomodoroActive] = useState<boolean>(true);
+  const [isBreakActive, setIsBreakActive] = useState<boolean>(false);
+  const [pomodoroCycles, setPomodoroCycles] = useState<number>(0);
+  const [modalIsOpen, setIsOpen] = useState<boolean>(false);
+  const [alarmSource, setAlarmSource] = useState<string>(
+    "/alarms/small-gong.mp3"
+  );
 
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
-  const handleActiveInactiveButtons = (name: string) => {
+  const handleChangesPomodoroBreak = (name: string) => {
     setIsRunning(false);
 
     if (name === "pomodoro") {
@@ -156,13 +131,13 @@ function Home() {
       setPomodoroCycles((prevPomodoroCycles) => {
         const updatedCycles = prevPomodoroCycles + 1;
 
-        handleActiveInactiveButtons("break");
+        handleChangesPomodoroBreak("break");
         resetTimer("break", updatedCycles);
 
         return updatedCycles;
       });
     } else {
-      handleActiveInactiveButtons("pomodoro");
+      handleChangesPomodoroBreak("pomodoro");
       resetTimer("pomodoro");
     }
   };
@@ -192,7 +167,7 @@ function Home() {
             [styles["button-inactive"]]: !isPomodoroActive
           })}
           onClick={() => {
-            handleActiveInactiveButtons("pomodoro");
+            handleChangesPomodoroBreak("pomodoro");
             setPomodoroTimer();
           }}
         >
@@ -204,7 +179,7 @@ function Home() {
             [styles["button-inactive"]]: !isBreakActive
           })}
           onClick={() => {
-            handleActiveInactiveButtons("break");
+            handleChangesPomodoroBreak("break");
             setBreakTimer();
           }}
         >
---
+++ src/utils/constants.ts
@@ -0,0 +1,50 @@
+export const DEFAULT_POMODORO_MINUTE = 1;
+export const DEFAULT_POMODORO_SECOND = 6;
+export const DEFAULT_SHORT_BREAK_MINUTE = 0;
+export const DEFAULT_SHORT_BREAK_SECOND = 5;
+export const DEFAULT_LONG_BREAK_MINUTE = 0;
+export const DEFAULT_LONG_BREAK_SECOND = 10;
+
+type Alarm = {
+  id: string;
+  name: string;
+  src: string;
+};
+
+export const alarmsMap: Alarm[] = [
+  {
+    id: "e3b0c442-98fc-462d-a1e8-3bf0bb407ad5",
+    name: "Birds chirping",
+    src: "/alarms/birds-chirping.mp3"
+  },
+  {
+    id: "d0f5c79b-7bfb-4d1c-9837-4a7ffab39175",
+    name: "Game show winner",
+    src: "/alarms/game-show-winner-bell.mp3"
+  },
+  {
+    id: "4d92517f-e30b-4f19-9e84-0047d9c94d7e",
+    name: "Hotel desk bell",
+    src: "/alarms/hotel-desk-bell.mp3"
+  },
+  {
+    id: "0e7cfb6a-87a5-4374-a3cc-2a1ecbb2ef73",
+    name: "Large gong",
+    src: "/alarms/large-gong.mp3"
+  },
+  {
+    id: "c84f98d7-93f0-49d2-8c58-752a767d2402",
+    name: "Small gong",
+    src: "/alarms/small-gong.mp3"
+  },
+  {
+    id: "617bdd67-589f-4e27-94ad-d8d3994c1c29",
+    name: "Police siren",
+    src: "/alarms/police-siren.mp3"
+  },
+  {
+    id: "89e2a07f-fc7d-4c56-bc5a-024d21bbfc57",
+    name: "Train steam whistle",
+    src: "/alarms/train-steam-whistle.mp3"
+  }
+];
